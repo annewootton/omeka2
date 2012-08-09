4935746c091fb94499a13970999b0473c5dd2499
Anne Wootton <anne.wootton@gmail.com>
2012-08-09 12:20:10 -0700

    First Commit

---
+++ themes/BeatlesLiveTheme/index.php
@@ -0,0 +1,10 @@
+<?php head(array('bodyid'=>'home')); ?>
+
+<div id="primary-index">
+
+	<div class= "youtube"><iframe width="600" height="450" src="http://www.youtube.com/embed/7pQPjpX82z8" frameborder="0" allowfullscreen></iframe></div>
+ 
+    
+</div><!-- end primary -->
+
+<?php foot(); ?>
